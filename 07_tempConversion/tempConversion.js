896726d9d43e476912b7d2a8501b6df5364f235d
jochuu <reyn@live.co.uk>
2021-08-11 16:05:51 +0100

    Fix first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+/* 
+* ftoc(32) // fahrenheit to celsius, should return 0
+* ctof(0) // celsius to fahrenheit, should return 32
+*/
 
+const ftoc = function(farenheit) {
+  return farenheit-32;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return celsius+32;
 };
 
 module.exports = {
