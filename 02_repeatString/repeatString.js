2bc7620574501de2251a947591b0cd6286028d65
jochuu <reyn@live.co.uk>
2021-08-11 11:48:23 +0100

    Fix rest of tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
 const repeatString = function(string, times) {
     // Recursion
     if (times < 0) {
+        return 'ERROR';
+    }
+    if (times === 0) {
         return '';
     }
     if (times === 1) {
