27cf11bae2adb38432d7df832ddf54e8a6a36622
jochuu <reyn@live.co.uk>
2021-08-11 15:54:46 +0100

    Fix all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
 const sumAll = function(min, max) {
     let sum = 0;
+
+    if (min < 0 || max < 0) { //Negative number check
+        return "ERROR";
+    } 
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)) { //Integer check
+        return "ERROR";
+    } 
+
+    if(max < min) { //Swap min with max and max with min
+        [min, max] = [max, min]
+    }
+
     for (let i = min; i <= max; i++) {
         sum += i;
     }
