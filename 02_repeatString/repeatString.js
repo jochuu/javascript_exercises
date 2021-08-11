ae0a133cef89efec299a62f0b0651059904fc524
jochuu <reyn@live.co.uk>
2021-08-11 11:46:26 +0100

    Fix 2nd test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-    return 'heyheyhey';
+const repeatString = function(string, times) {
+    // Recursion
+    if (times < 0) {
+        return '';
+    }
+    if (times === 1) {
+        return string;
+    }
+    else {
+        return string + repeatString(string, times - 1);
+    }
 };
 
 module.exports = repeatString;
