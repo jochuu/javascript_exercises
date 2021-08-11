73bb770d9a668d411cde696018bb3ede10e1a225
jochuu <reyn@live.co.uk>
2021-08-11 16:15:05 +0100

    Fix all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,16 @@
 /* 
-* ftoc(32) // fahrenheit to celsius, should return 0
-* ctof(0) // celsius to fahrenheit, should return 32
+* fahrenheit to celsius - subtract 32 and multiply by 5/9
+* celsius to fahrenheit - multiply by 9/5 and add 32
+* multiply by 10 and divide by 10 to get 1 dp
 */
 
 const ftoc = function(farenheit) {
-  return farenheit-32;
-};
+  return Math.round(((farenheit - 32) * (5/9) * 10) / 10);
+}
 
 const ctof = function(celsius) {
-  return celsius+32;
-};
+  return Math.round((((celsius * 9/5) + 32) * 10) / 10);
+}
 
 module.exports = {
   ftoc,
