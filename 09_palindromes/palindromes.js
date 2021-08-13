4bcecaf3338c9fac523c234d0b42f84cb4949ddc
jochuu <reyn@live.co.uk>
2021-08-13 19:18:03 +0100

    Fix first test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reversedString = string.split("").reverse().join("");
+    return (string === reversedString);
 };
 
 module.exports = palindromes;
