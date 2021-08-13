125a16d54a9af0a19a43cc2058eb20d7cdddc93c
jochuu <reyn@live.co.uk>
2021-08-13 19:13:06 +0100

    Add rest of functions and pass tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,29 +1,32 @@
-const add = function(...args) {
-  let add = 0;
-    for (let i = 0; i < args.length; i++) {
-      add += args[i];
-    }
-  return add;
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) return 0;
+  return array.reduce((totalValue, currentItem) => totalValue + currentItem);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) return 0;
+  return array.reduce((totalValue, currentItem) => totalValue * currentItem);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let product = 1;
+  if (number === 0) return product;
+  for (let i = 1; i <= number; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 module.exports = {
