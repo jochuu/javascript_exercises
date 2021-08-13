9dd78ebc6337beebd97df21d15c4d96ee02c3ed1
jochuu <reyn@live.co.uk>
2021-08-13 19:55:04 +0100

    Fix tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    let previousValue = 0;
+    let currentValue;
+    let nextValue = 1;
+    for (let i = 1; i <= index; i++) {
+        currentValue = nextValue;
+        nextValue = nextValue + previousValue;
+        previousValue = currentValue;
 
+        // cV = 1; nV = 1 + 0; pV = 1;  index = 1
+        // cV = 1; nV = 1 + 1; pV = 1;  index = 2
+        // cV = 2; nV = 1 + 2; pV = 2;  index = 3
+        // cV = 3; nV = 2 + 3; pV = 3;  index = 4
+        // cV = 5; nV = 3 + 5; pV = 5;  index = 5
+        // cV = 8; nV = 5 + 8; pV = 8;  index = 6
+    }
+    return currentValue;
 };
 
 module.exports = fibonacci;
