1abd8f2bfb05bdb522b8f83d8c9d196b54f3d4b4
jochuu <reyn@live.co.uk>
2021-08-13 21:27:15 +0100

    Fix tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((a, b) => {
 
-};
+        if (!a.yearOfDeath) {a.yearOfDeath = new Date().getFullYear();}
+        if (!b.yearOfDeath) {b.yearOfDeath = new Date().getFullYear();}
+        
+        let oldestAge = a.yearOfDeath - a.yearOfBirth;
+        let currentAge = b.yearOfDeath - b.yearOfBirth;
+        
+        if (oldestAge < currentAge) {
+            return b;
+        } else {
+            return a;
+        }
+    })
+  };
 
 module.exports = findTheOldest;
